9f6ef8bd8b9b836630845973dc3a790922606900
Kyle Brown <picokyle@gmail.com>
2016-09-08 22:02:48 -0600

    Allow adding new flash cards

--- app/controllers/flash_cards_controller.rb
+++ app/controllers/flash_cards_controller.rb
@@ -5,4 +5,16 @@ class FlashCardsController < ApplicationController
 
   def new
   end
+
+  def create
+    @flash_card = FlashCard.new(flash_card_params)
+    @flash_card.save
+    redirect_to action: 'index'
+  end
+
+  private
+
+  def flash_card_params
+    params.require(:flash_card).permit(:question, :answer)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :flash_cards
+
   root 'flash_cards#index'
 end
