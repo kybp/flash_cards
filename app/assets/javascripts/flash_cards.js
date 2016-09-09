d6ae7fb1040e1065d75d710376a8f79ad016f3db
Kyle Brown <picokyle@gmail.com>
2016-09-09 15:30:25 -0600

    Show cards one after another with a timer

--- app/assets/javascripts/flash_cards.js
+++ app/assets/javascripts/flash_cards.js
@@ -1,10 +1,42 @@
-const flashCardController = ['$http', '$scope', function($http, $scope) {
+const flashCardController =
+['$http', '$scope', '$timeout', function($http, $scope, $timeout) {
+  const SECONDS_PER_CARD = 10
+
   $http.get('/flash_cards.json')
     .then(function(response) {
       $scope.cards = response.data
     }, function(response) {
       alert('error fetching cards: ' + response.status)
     })
+
+  $scope.beganReview = false
+  $scope.doneReview  = false
+
+  $scope.beginReview = function() {
+    $scope.beganReview  = true
+    $scope.currentIndex = 0
+    $scope.secondsLeft  = SECONDS_PER_CARD
+    let timeout
+
+    $scope.onTimeout = function() {
+      const cardsRemaining = $scope.currentIndex + 1 < $scope.cards.length
+      --$scope.secondsLeft
+
+      if (cardsRemaining && $scope.secondsLeft === 0) {
+        ++$scope.currentIndex
+        $scope.secondsLeft = SECONDS_PER_CARD
+      }
+
+      if (cardsRemaining) {
+        timeout = $timeout($scope.onTimeout, 1000)
+      } else {
+        $timeout.cancel(timeout)
+        $scope.doneReview = true
+      }
+    }
+
+    timeout = $timeout($scope.onTimeout, 1000)
+  }
 }]
 
 angular
